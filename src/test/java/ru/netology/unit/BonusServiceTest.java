a64f027dfa7402731430d7a81460df680c580894
Dmitry Trofimov <dm.trofimov@corp.vk.com>
2021-11-25 22:48:11 +0300

    change tests for junit4

--- src/test/java/ru/netology/unit/BonusServiceTest.java
+++ src/test/java/ru/netology/unit/BonusServiceTest.java
@@ -1,12 +1,11 @@
 package ru.netology.unit;
 
-import org.junit.jupiter.api.Test;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-class BonusServiceTest {
+public class BonusServiceTest {
     @Test
-    void shouldReturn10IfAmountIs2000() {
+    public void shouldReturn10IfAmountIs2000() {
         BonusService service = new BonusService();
         int amount = 2000;
 
@@ -17,9 +16,20 @@ class BonusServiceTest {
     }
 
     @Test
-    void shouldReturnZeroIfAmountLowerThan1000() {
+    public void shouldReturnZeroIfAmountLowerThan1000() {
         BonusService service = new BonusService();
         int amount = 900;
+
+        int actual = service.calculateBonus(amount);
+        int expected = 0;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldReturnZeroIfAmountLowerThan1000() {
+        BonusService service = new BonusService();
+        int amount = 1000;
 
         int actual = service.calculateBonus(amount);
         int expected = 0;
